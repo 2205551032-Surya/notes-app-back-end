e2bf66f57ee4b521a7bd5e04167432b69aff00f4
I Putu Gede Surya Pratama <suryapratamaigede123@gmail.com>
2024-11-01 14:02:18 +0800

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+
+export default [
+  daStyle,
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+
+{
+  "env": {
+      "commonjs": true,
+      "es2021": true,
+      "node": true
+  },
+  "extends": [
+      "airbnb-base"
+  ],
+  "parserOptions": {
+      "ecmaVersion": 12
+  },
+  "rules": {
+      "no-console": "off"
+  }
+}
+
+];
